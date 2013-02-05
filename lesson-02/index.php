55b0cf7d08c5400d3c5fa1d4bc5fa92a657f4848
jscheer <joseph_scheer@comcast.net>
2013-02-05 02:55:28 -0800

    add PHP input form with date calculator

---
+++ lesson-02/index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>CS 53.11b Week 2 - php</title>
+<style type="text/css">
+body,td,th {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: medium;
+}
+</style>
+</head>
+<body>
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+?>
+<?php
+    function age($birthdate) {
+        return (strtotime('now') - strtotime($birthdate)) / (60*60*24*365);
+    }
+    $age = age($_POST['birthdate']);
+    if ($age > 21) {
+?>
+<p>You are 21 or over.</p>       
+<?php	
+    } else {
+?>
+<p>You are not 21 yet.<?php echo intval($age) ?></p>        
+<?php
+    }       
+} else {
+?>
+<form action="" method="post">
+    <p>Please enter your birthdate so we can verify you are 21.</p>
+    <input name="birthdate" type="date">
+    <input name="" type="submit">
+</form>   
+<?php
+}
+?>
+</body>
+</html>
